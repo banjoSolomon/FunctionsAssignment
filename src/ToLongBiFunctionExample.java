8a2df669029f55ccc7a08615822747fef13f57fb
banjoSolomon <ayomidebanjo68@gmail.com>
2024-05-20 09:17:22 +0100

    update

--- src/ToLongBiFunctionExample.java
+++ src/ToLongBiFunctionExample.java
@@ -4,5 +4,6 @@ public class ToLongBiFunctionExample {
     public static void main(String[] args) {
         ToLongBiFunction<Integer, Integer> toLongBiFunction = (a, b) -> (long) a * b;
         System.out.println(toLongBiFunction.applyAsLong(1, 2));
+
     }
 }
