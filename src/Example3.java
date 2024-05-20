9feeee4570b817793c44c30852f9e6c792e673b8
banjoSolomon <ayomidebanjo68@gmail.com>
2024-05-20 16:53:27 +0100

    update

--- src/Example3.java
+++ src/Example3.java
@@ -3,7 +3,6 @@ import java.util.stream.IntStream;
 public class Example3 {
     public static void main(String[] args) {
         String words = "Hello, world";
-        IntStream intStream = words.chars();
-        intStream.forEach(System.out::println);
+        words.chars().filter((number)->number>100).map((number)->number*number).forEach(System.out::println);
     }
 }
