e88f2f59deef5d5202a20e86a6a64afe597abe3b
banjoSolomon <ayomidebanjo68@gmail.com>
2024-05-21 12:06:17 +0100

    update

---
+++ src/Example11.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Example11 {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(10, 20, 30, 40);
+       var start = numbers.stream().flatMapToInt((element)-> IntStream.of(element*2)).sum();
+        System.out.println(start);
+
+
+    }
+}
